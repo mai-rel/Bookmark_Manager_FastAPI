05d7965e341151e9c962c6a2606a67e15089878e
Mairel Veys <marylight.music@yandex.ru>
2026-07-26 18:16:16 +0300

    refactor: add async database configuration

--- app/db.py
+++ app/db.py
@@ -1,15 +1,23 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base, DeclarativeBase
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+
 
 DATABASE_URL = "sqlite:///./bookmarks.db"
+ASYNC_DATABASE_URL = "sqlite+aiosqlite:///./bookmarks.db"
+
 
 engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False}
 )
+async_engine = create_async_engine(ASYNC_DATABASE_URL)
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, 
 bind=engine)
+async_session = async_sessionmaker(bind=async_engine, class_=AsyncSession, expire_on_commit=False)
+
 
 class Base(DeclarativeBase):
     pass
